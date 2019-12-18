835d62ae26e11310e208a42e84562c7739000aa6
cesar mota <cesarmota@ymail.com>
2019-12-18 20:03:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,15 +5,18 @@ p sum
 end
 
 def subtraction(num1, num2)
-
+difference=num1-num2
+p difference
 end
 
 def division(num1, num2)
-
+quotient=num1/num2
+p quotient
 end
 
 def multiplication(num1, num2)
-
+product=num1*num2
+p product
 end
 
 def modulo(num1, num2)
